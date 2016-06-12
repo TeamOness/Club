5af2ca2ac12c399f70284b37f7bfdbf442648de1
niuliang <yesuloveniuniu@sina.com>
2016-06-12 14:16:21 +0800

    main-local.php

---
+++ admin/common/config/main-local.php
@@ -0,0 +1,22 @@
+<?php
+
+$config = [
+	'defaultRoute' => 'login',
+    'components' => [
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => 'AnDuJPyrsCDMwZpObzO-rNx8WN94Z_KX',
+        ],
+    ],
+];
+
+if (!YII_ENV_TEST) {
+    // configuration adjustments for 'dev' environment
+    $config['bootstrap'][] = 'debug';
+    $config['modules']['debug'] = 'yii\debug\Module';
+
+    $config['bootstrap'][] = 'gii';
+    $config['modules']['gii'] = 'yii\gii\Module';
+}
+
+return $config;
