d7cbad3e1e2401f26976484914facd21dbcbefc3
崔阳阳 <2650991021@qq.com>
2016-06-12 19:55:57 +0800

    666

--- app/Http/Controllers/CommonController.php
+++ app/Http/Controllers/CommonController.php
@@ -7,7 +7,12 @@ use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use DB;
-use Request;
+//use Request;
+use Illuminate\Http\Request;
+use App\Http\Requests;
+use App\User;
+use App\Http\Controllers\Controller;
+
 
 class CommonController extends Controller
 {
@@ -20,11 +25,11 @@ class CommonController extends Controller
 	//分期购车添加成功页面
 	public function commonok(request $request){
 		//接收表单提交过来的值
-		$buy_name=Request::post('buy_name');
-		echo $buy_name;die;
-		$buy_money_date=$_POST['buy_money_date'];
-		$buy_phone=$_POST['buy_phone'];
-		$add=DB::insert('insert into buy (buy_name,buy_money_date,buy_phone) values (?, ?,?)', [$buy_name,$buy_money_date,$buy_phone]);
+		$buy_money=$request->input('buy_money');
+		//echo $buy_name;die;
+		$buy_money_date=$request->input('buy_money_date');
+		$buy_phone=$request->input('buy_phone');
+		$add=DB::insert('insert into buy (buy_money,buy_money_date,buy_phone) values ( ?,?,?)', [$buy_money,$buy_money_date,$buy_phone]);
 		//判断
 		if($add){
 			echo "<script>alert('提交成功');location.href='common'</script>";
